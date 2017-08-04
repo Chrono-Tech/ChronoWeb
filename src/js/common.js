514b8224187c434c23d8a9e6d870cabdd1e6aa1b
Tagir Yusupov <tagir.yusupov@gmail.com>
2017-08-04 15:49:38 +0300

    marque

--- src/js/common.js
+++ src/js/common.js
@@ -7,6 +7,7 @@ var TweenLite = require('gsap').TweenLite;
 var ScrollToPlugin = require('gsap/ScrollToPlugin');
 var Swiper = require('swiper');
 require("../../node_modules/jquery-validation/dist/jquery.validate.js");
+require("../../node_modules/jquery.marquee/jquery.marquee.min.js");
 
 
 
@@ -232,7 +233,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
+  $('.widjet').marquee({
+      //speed in milliseconds of the marquee
+      duration: 15000,
+      //gap in pixels between the tickers
+      gap: 50,
+      //time in milliseconds before the marquee will start animating
+      delayBeforeStart: 0,
+      //'left' or 'right'
+      direction: 'left',
+      //true or false - should the marquee be duplicated to show an effect of continues flow
+      // duplicated: true
+  });
 
 
 
